e0c9dc90c716ab33dd7db950510686910a46b77c
oleg <o.zagoruy@lms-service.ru>
2023-12-11 11:56:34 +0300

    first

--- my-app/src/Components/Search/Search.tsx
+++ my-app/src/Components/Search/Search.tsx
@@ -7,16 +7,13 @@ import {useLocation, useNavigate} from "react-router-dom";
 const Search = () => {
   const { value, setValue } = useInputValue( props => props);
   const setData = useStore(state => state.setData);
-  // const data = useStore(state => state.data);
-  // const [numberPage, setNumberPage] = useState(1);
   const location = useLocation();
   const navigate = useNavigate();
-  // let number = 1;
   
   /**@description initial set data*/
   const handlerSetData = async () => {
-    const {results} = await getData(value, 30);
     navigateMain();
+    const {results} = await getData(value, 30);
     setData( results);
   }
   const submit = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -25,37 +22,6 @@ const Search = () => {
     }
   }
   const navigateMain = () => location.pathname !== '/main' && navigate('/main');
-  
-  // const scroll = () => {
-  //   const scrollOfTop = Math.trunc(window.pageYOffset);
-  //   const windowHeight = window.innerHeight;
-  //   const scrollHeight =  Math.max(
-  //     document.body.scrollHeight, document.documentElement.scrollHeight,
-  //     document.body.offsetHeight, document.documentElement.offsetHeight,
-  //     document.body.clientHeight, document.documentElement.clientHeight
-  //   );
-  //
-  //   if (scrollHeight - windowHeight - scrollOfTop <= 90) {
-  //     number += 1;
-  //     setNumberPage(number);
-  //   }
-  // };
-  //
-  // useEffect(()=> {
-  //   document.addEventListener('scroll',scroll);
-  //   return ()=> {
-  //     document.removeEventListener('scroll', scroll)
-  //   }
-  // }, []);
-  //
-  // const handlerSetData2 = async () => {
-  //   const {results} = await getData(value, 30, numberPage);
-  //   setData( [...data, ...results]);
-  // }
-  
-  // useEffect(() => {
-  //   void handlerSetData2();
-  // }, [numberPage]);
   
   return (
       <div className="search-control">
--- my-app/src/Pages/Start/Start.tsx
+++ my-app/src/Pages/Start/Start.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Search from '../../Components/Search/Search';
+import './Start.scss';
 
 const Start = () => {
   return (
     <div className={'container page-start'}>
-      <Search/>
+      <div>
+        <Search/>
+      </div>
     </div>
   );
 };
